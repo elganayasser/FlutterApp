6dba8a6e226c42effc090f6b97470ad8ba4b8b5e
elgan <elgana@gmail.com>
2020-05-17 20:33:20 +0000

    Design updated

--- login_demo/lib/login_page.dart
+++ login_demo/lib/login_page.dart
@@ -70,34 +70,75 @@ FormType _formType = FormType.login;
 @override
   Widget build(BuildContext context) {
    
-    return new Scaffold(
-      appBar : new AppBar(title: 
-      new Text('Flutter login'),
-      ),
+    return  SafeArea(
+      
+      child: Scaffold(
+      
+  
+       backgroundColor: Color(0xff0D192A),
       body: new Container(
-        padding: EdgeInsets.all(15.0),
-        child : new Form(
-          key : formKey,
-          child: new Column(
+        child: ListView(children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(10.0),
+              child: Container(
+                
+                width: 120,
+                height: 120,
+                margin: EdgeInsets.fromLTRB(0, 50, 0, 0),
+            
+               
+            ),
+              ),
+               Padding(
+              padding: EdgeInsets.all(5.0),
+              child: Container(
+                margin: EdgeInsets.all(5.0),
+                child: Center(
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+             Padding(
+              padding: EdgeInsets.all(40.0),
+
+             child : new Form(key : formKey,  child: new Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: buildInputs()+buildButtons() ,
-            )
-            ,)
+            ))
+             ),
+              
+        ],
         
-        ),
+        
+        
+    ),
+    ) ,
+      ),
     );
   }
   List<Widget> buildInputs(){
     return [
            new TextFormField(
-            decoration : new InputDecoration (labelText: 'Email'),
+             style: TextStyle(color: Colors.black),
+            decoration : new InputDecoration (labelText: 'Email', fillColor: Colors.white, filled: true, hintText: "Enter Your Email",
+                          hintStyle: TextStyle(color: Colors.grey[400]), border: InputBorder.none ),
            validator: (value) => value.isEmpty ? 'Email can\'t be empty': null,
    
             onSaved: (value) => _email = value,
 
           ),
+          SizedBox(height: 20.0),
           new TextFormField(
-            decoration : new InputDecoration (labelText: 'Password'),
+             
+            style: TextStyle(color: Colors.black),
+            decoration : new InputDecoration (labelText: 'Password', fillColor: Colors.white, filled: true, hintText: "Enter Your Email",
+                          hintStyle: TextStyle(color: Colors.grey[400]), border: InputBorder.none ),
             obscureText: true,
              validator: (value) => value.isEmpty ? 'Password can\'t be empty': null,
              onSaved: (value) => _password = value,
@@ -106,17 +147,24 @@ FormType _formType = FormType.login;
   }
 
   List<Widget> buildButtons(){
+     
     if(_formType == FormType.login){
 
     
 return[
-   new RaisedButton(
+   SizedBox(height: 20.0),
+   new FlatButton(
+      
             child : new Text('Login',style : new TextStyle(fontSize: 21.0)),
             onPressed: validateAndSubmit,
+            padding: EdgeInsets.all(10.0),
+              color: Colors.blue,
+              textColor: Colors.white
           ),
-          new FlatButton(
+          new RaisedButton(
             child: new Text('Create an Account' , style: new TextStyle(fontSize : 21.0)),
             onPressed: moveToregistre,
+            
             )
 ];
 } else{

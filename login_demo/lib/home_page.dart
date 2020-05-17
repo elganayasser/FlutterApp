65fcfacbc14b96b6309cebda436b7208956da3ac
elgan <elgana@gmail.com>
2020-05-17 04:29:13 +0000

    almost

--- login_demo/lib/home_page.dart
+++ login_demo/lib/home_page.dart
@@ -1,20 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:login_demo/auth.dart';
+import 'package:login_demo/login_page.dart';
 
 class HomePage extends StatelessWidget {
 HomePage({this.auth,this.onSignedOut});
 final BaseAuth auth;
 final VoidCallback onSignedOut;
-
-void _signOut() async{
-  try{
-    await auth.signOut();
-    onSignedOut();
-
-  }catch(e){
- print(e);
-  }
+Future navigateToSubPage(context) async {
+  Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
 }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +18,11 @@ void _signOut() async{
     return new Scaffold(
       appBar : new AppBar (title: new Text('Cap App '),
       actions: <Widget>[
-        new FlatButton(onPressed: _signOut, 
+        new FlatButton(onPressed: (){
+            auth.signOut();
+    navigateToSubPage(context);
+  
+        }, 
         child: new Text('Logout', style : new TextStyle(fontSize:16.0 , color : Colors.redAccent )))
       ],
       ),
